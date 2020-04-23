50dad834358ac62ac0287d4ae36860762456c0ab
brandonjones085 <34459797+brandonjones085@users.noreply.github.com>
2020-04-23 16:39:35 -0400

    Add files via upload

--- jonewill.buildrooms.c
+++ jonewill.buildrooms.c
@@ -1,78 +1,72 @@
-// rooms
-// creates file that hold descriptions of rooms and how they're connected
 
-// create directory jonewill.rooms.PID
-
-// generate 7 rooms files
 
 #include <sys/stat.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include <unistd.h>  //pid
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <time.h>
-#include <assert.h>
-#include <fcntl.h> 
+#include <stdbool.h> //for bools
+#include <time.h>  //random
 
 
 char dir[20];
 int* pid; 
 
 
-typedef struct Room 
+struct Room 
 {
     char* name; 
     char* type; 
     int numOutbound;  
     struct Room* outboundCon[6]; 
-} Room; 
+};
 
 
 
 void shuffleArray(char **array, int n); 
 void createDir(int pid);
-bool sameRoom(Room* x, Room* y);
-bool canAddOutboundFrom(Room* x);
-void connectRooms(Room* x, Room* y);
-void addRandomConnection(Room* room);
-void createFiles(int pid, Room* roomArray);
-bool connectionExists(Room* x, Room* y); 
-void outputFiles(Room* rooms); 
-bool isGraphComplete(Room* rooms); 
+bool sameRoom(struct Room* x, struct  Room* y);
+bool canAddConnectionFrom(struct Room* x);
+void connectRoom(struct Room* x,struct  Room* y);
+void addRandomConnection(struct Room* room);
+void createFiles(int pid,struct  Room* roomArray);
+bool connectionExists(struct Room* x, struct Room* y); 
+bool isGraphFull(struct Room* rooms); 
+void generateGraph(struct Room* rooms); 
 
-void generateGraph(Room* rooms); 
-
-
+//Hard coded file names
 const char *fileNames[7] =
 {
-    "first room", 
-    "second room", 
-    "third room", 
-    "fourth room", 
-    "fifth room", 
-    "sixth room", 
-    "seventh room"
+    "First_File", 
+    "Second_File", 
+    "Third_File", 
+    "Fourth_File", 
+    "Fifth_File", 
+    "Sixth_File", 
+    "Seventh_File"
 };
 
-
+//List of 10 rooms names
 char *roomNames[10] = 
 {
-    "one", 
-    "two", 
-    "three", 
-    "four", 
-    "five", 
-    "six", 
-    "seven", 
-    "eight", 
-    "nine", 
-    "ten"
+    "Living Room", 
+    "Master Bedroom", 
+    "Office", 
+    "Kitchen", 
+    "Bathroom 1", 
+    "Guest Bedroom", 
+    "Bathroom 2", 
+    "Front Porch", 
+    "Garage", 
+    "Basement"
 };
 
-
-
+//--------------------------------------------------------
+//Name: shuffleArray
+//Parameters: The array and number of items in array
+//Output: Returns the shuffled array
+//Description: Used to shuffle array to randomize
+//--------------------------------------------------------
 
 void shuffleArray(char **array, int n)
 {
@@ -88,7 +82,12 @@ void shuffleArray(char **array, int n)
     }
 }
 
-// Creates directory 
+//--------------------------------------------------------
+//Name: CreateDir
+//Parameters: Takes in the PID 
+//Output: Returns the new directory
+//Description: Creates a new directory using the jonewill.rooms.<PID>
+//--------------------------------------------------------
 void createDir(int pid)
 {
     sprintf(dir, "jonewill.rooms.%d", pid); 
@@ -97,10 +96,17 @@ void createDir(int pid)
     
 }
 
-bool sameRoom(Room* x, Room* y)
+
+//--------------------------------------------------------
+//Name: sameRoom
+//Parameters: Two room structs
+//Output: Returns true if they're the same, false if they're different
+//Description: Compares two room names. Returns true if they're the same
+//--------------------------------------------------------
+bool sameRoom(struct Room* x, struct Room* y)
 {
-    assert(x && y); 
-    if (x == y)
+    //compares the name
+    if (x->name == y->name)
     {
         return true; 
     }
@@ -111,8 +117,13 @@ bool sameRoom(Room* x, Room* y)
     
 }
 
-
-bool canAddOutboundFrom(Room* x)
+//--------------------------------------------------------
+//Name: canAddOutboundFrom
+//Parameters: single room 
+//Output: Returns true if outbound connections are less than 6
+//Description: Looks at number of outbound connections. True is < 6. False if more than 6
+//--------------------------------------------------------
+bool canAddConnectionFrom(struct Room* x)
 {
 
     if (x->numOutbound < 6)
@@ -122,150 +133,214 @@ bool canAddOutboundFrom(Room* x)
     else
     {
         return false; 
-    }
-    
-    
+    }   
 }
 
-
-bool connectionExists(Room* x, Room* y)
+//--------------------------------------------------------
+//Name: connectionExists
+//Parameters: two room structs
+//Output: Returns true they are already connected, false if not
+//Description: compares the two rooms and checks if there is already a connection. True if one is found, false if not
+//--------------------------------------------------------
+bool connectionExists(struct Room* x, struct Room* y)
 {
-    assert(x && y); 
     int i = 0; 
-    for (i = 0; i < x->numOutbound; i++)
+    while (i < x->numOutbound)
     {
-        if (x->outboundCon[i] == y) return true; 
+        //each connection for x
+        if (x->outboundCon[i] == y) 
+        {
+            return true; //connection found 
+        }
+        i++; 
     }
+
+    //if not found
     return false; 
 
 }
 
-void connectRooms(Room* x, Room* y)
+
+//--------------------------------------------------------
+//Name: connectRoom
+//Parameters: two room structs
+//Output: Room x points to room y
+//Description: Used to point room x to room y. Used while creating the graph
+//--------------------------------------------------------
+void connectRoom(struct Room* x, struct Room* y)
 {
-    if (canAddOutboundFrom(x) || canAddOutboundFrom(y))
+    int num = x->numOutbound; //gets num
+    if (canAddConnectionFrom(x) )
     {
-        x->outboundCon[x->numOutbound++] = y; 
-        y->outboundCon[y->numOutbound++] = x; 
+        x->outboundCon[num] = y; //creates link
+        x->numOutbound += 1; //increments int by one
     }
     
 }
 
 
-
-void addRandomConnection(Room* room)
+//--------------------------------------------------------
+//Name: addRandomConnection
+//Parameters: the room array 
+//Output: Two rooms linked
+//Description: Used to while creating the graph. 
+//finds two random rooms by index. 
+//Sends two rooms to the connect function. 
+//--------------------------------------------------------
+void addRandomConnection(struct Room* room)
 {
-    assert(room); 
-    Room* x; 
-    Room* y; 
+  
+    struct Room* x; 
+    struct Room* y; 
+
+    srand(time(NULL)); 
+    int ind1; 
+    int ind2;
+
+    do
+    {
+        ind1 = rand() % 7; //random ind between 0 and 6
+        x = &room[ind1]; 
+    }while (canAddConnectionFrom(x) == false); 
 
     do 
     {
-        x = &room[rand() % 7]; 
-    }while (!canAddOutboundFrom(x)); 
+        ind2 = rand() % 7; //random ind between 0 and 6
+        y = &room[ind2]; 
+        //validates a connection can be made, not the same room, and connection doesn't exist
+    }while (canAddConnectionFrom(y) == false || sameRoom(x, y) == true || connectionExists(x, y) == true );
 
-    do 
-    {
-        y = &room[rand() % 7]; 
-    }while (!canAddOutboundFrom(y) || sameRoom(x, y) || connectionExists(x, y));
-
-    connectRooms(x,y); 
+    connectRoom(x,y); 
+    connectRoom(y,x);
 }
 
-
-bool isGraphComplete(Room* rooms)
+//--------------------------------------------------------
+//Name: isGraphFull
+//Parameters: the room array 
+//Output: True if full, false if not
+//Description: Checks if each room index has between 3 and 6 connections
+//--------------------------------------------------------
+bool isGraphFull(struct Room* rooms)
 {
-    int i, con;
-    for (i = 0; i < 7; i++) {
-        con = rooms[i].numOutbound;
-        if (con < 3 || con > 6)
-            return false;
+    int i = 0;
+    int con;
+
+    while (i < 7)
+    {
+        con = rooms[i].numOutbound;  //num for each room
+        if (con < 3)                 //less than 3
+        {
+            return false; 
+        }
+        else if (con > 6)           //greater than 3
+        {
+            return false; 
+        }
+        i++; 
     }
 
-
-    return true;
+    return true;                 //full returns true
 }
 
-void createFiles(int pid, Room* rooms)
+
+//--------------------------------------------------------
+//Name: createFiles
+//Parameters: the pid, and roomArray
+//Output: All the contents of the roomArray into each file
+//Description: Creates the files and outputs the name, connections, and type of room to the files. 
+//--------------------------------------------------------
+void createFiles(int pid, struct Room* rooms)
 {
     int i, j, r; 
   
     char file[100];
      
-    sprintf(dir, "jonewill.rooms.%d", pid); 
+    sprintf(dir, "jonewill.rooms.%d", pid); //dir to moves files into 
 
-    for (i = 0; i < 7; i++)
+    for (i = 0; i < 7; i++)   //loops through each ind of the array 
     {
         
-     
         char file[100];
-        for (j = 0; j < 100; j++) file[j] = '\0'; 
+        for (j = 0; j < 100; j++) file[j] = '\0'; //initalizes each filename
         {
-        srand(time(NULL)); 
-
+       
+        //creates string for file name with dir
         strcat(file, dir); 
         strcat(file, "/"); 
         strcat(file, fileNames[i]); 
 
-        FILE* fileDescriptor = fopen(file, "w"); 
+        FILE* fileDescriptor = fopen(file, "w"); //opens
       
+        //appends to each file
         fprintf(fileDescriptor, "NAME: %s\n", roomNames[i]); 
-        
         fprintf(fileDescriptor, "Room Type: %s\n", rooms[i].type); 
       
-        
-        
-
          for (r = 0; r < rooms[i].numOutbound; r++)
         {
             fprintf(fileDescriptor, "Connection %d: %s\n", r+1, rooms[i].outboundCon[r]->name);
         }
-        fclose(fileDescriptor); 
+
+        fclose(fileDescriptor); //close file
         }
- 
-        
     }
 }
 
-
-void generateGraph(Room* rooms)
+//--------------------------------------------------------
+//Name: generateGraph
+//Parameters:  roomArray
+//Output: Creates the graph, assignes a name, and type of room
+//Description: Shuffles roomname array, assigns the first 7 ind to room name. 
+//Generates two random numbers which are used to create indexes and assign the type of room. 
+//passes array to random connection function to link other rooms. 
+//--------------------------------------------------------
+void generateGraph(struct Room* rooms)
 {
 
     int i, j; 
-    int randomNumber1 = 0;
+    int randomNumber1 = 0;  // random num for index to assign type of room
     int randomNumber2 = 0;
 
     srand(time(NULL)); 
+
+    //makes sure random nums are not the same
     while (randomNumber1 == randomNumber2)
     {
         randomNumber1 = (rand() % 7); 
         randomNumber2 = (rand() % 7); 
     }
 
-    shuffleArray(roomNames, 10);
+    shuffleArray(roomNames, 10);  //shuffles array
+
+    //first 7 ind assigned room names
     for (i = 0; i < 7; i++)
     {
-        rooms[i].name = roomNames[i];
-        if (i == randomNumber1)
+        rooms[i].name = roomNames[i];  //room name
+        if (i == randomNumber1)       //random number
         {
             rooms[i].type = "Start Room"; 
         } 
-        else if(i == randomNumber2)
+        else if(i == randomNumber2)     //random number
         {
             rooms[i].type = "End Room"; 
         }
         else
         {
-            rooms[i].type = "Mid Room"; 
+            rooms[i].type = "Mid Room";  //rest of rooms
         }
 
+        //Sets the initial number to zero 
         rooms[i].numOutbound = 0; 
-        for (j = 0; j < 6; j++) rooms[i].outboundCon[j] = NULL;
+
+        //Initalizes the outbound connection so the graph can be generated
+        for (j = 0; j < 6; j++) 
+        {
+            rooms[i].outboundCon[j] = NULL;  //used to initalize
+        }
+        
     }
 
-
-    
-
-    while(isGraphComplete(rooms) == false )
+    //takes the array, while graph is incomplete, a random connection will be created
+    while(isGraphFull(rooms) == false )
     {
         addRandomConnection(rooms);
     }
@@ -275,7 +350,7 @@ void generateGraph(Room* rooms)
 
 int main()
 {
-    Room roomArray[7]; 
+    struct Room roomArray[7]; 
     int pid = getpid(); 
     generateGraph(roomArray); 
     createDir(pid); 
